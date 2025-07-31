68145f810e16f043de4963b88456235a0c5f1adc
CDarts48 <cdartswebdev@gmail.com>
2025-07-31 17:50:04 -0600

    et tu

--- Recon/BruteForceSubdomains/Brutus.ts
+++ Recon/BruteForceSubdomains/Brutus.ts
@@ -6,32 +6,32 @@ This can be altered to include less common characters, like '-'
 
 Chinese, Arabic and Latin characters are als supported by some browsers, but not all.
 */
-const generateSubdomains = function(length) {
-const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-'.split('');
-let subdomains = charset;
-let subdomain;
-let letter;
-let temp;
+const generateSubdomains = function (length: number): string[] {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-'.split('');
+    let subdomains: string[] = charset;
+    let subdomain: string;
+    let letter: string;
+    let temp: string[];
 
-/*
-Time Complexity: O(n*m)
-n = length of string
-m = number of possible characters
-This is because we generate all possible combinations of the characters in the charset.
-*/
+    /*
+    Time Complexity: O(n*m)
+    n = length of string
+    m = number of possible characters
+    This is because we generate all possible combinations of the characters in the charset.
+    */
 
-for (let i = 1; i < length; i++) {
-    temp = [];
-    for (let k = 0; k < subdomains.length; k++) {
-        subdomain = subdomains[k];
-        for (let m = 0; m < charset.length; m++) {
-            letter = charset[m];
-            temp.push(subdomain + letter);
+    for (let i = 1; i < length; i++) {
+        temp = [];
+        for (let k = 0; k < subdomains.length; k++) {
+            subdomain = subdomains[k]!; // Non-null assertion since k is within bounds
+            for (let m = 0; m < charset.length; m++) {
+                letter = charset[m]!; // Non-null assertion since m is within bounds
+                temp.push(subdomain + letter);
+            }
         }
+        subdomains = temp; // Fix: should assign to subdomains, not subdomain
     }
-    subdomain = temp
-}
-return subdomains;
+    return subdomains;
 }
 
 const subdomains = generateSubdomains(4)
